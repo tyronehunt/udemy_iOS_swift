9a30d2fc19561c183dc6d9b719ce733c7f6d5620
Tyrone Hunt <tyrone.hunt@king.com>
2021-08-02 12:44:31 +0100

    xylophone app with multiple UIButton linked to one IBAction to play sound and modify button properties after click

---
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundTitle: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundTitle: String) {
+        let url = Bundle.main.url(forResource: soundTitle, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
+
+
